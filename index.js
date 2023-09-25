e87569fa9de271ae9d06ee4ba3a49556987717fc
Samuel Muli <samuelmuli95@gmail.com>
2023-09-25 17:38:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(item => item.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(item => item.startsWith(name))
+}
+
+function matchName(drivers, name){
+    return drivers.filter(item => item.name === name)
+}
